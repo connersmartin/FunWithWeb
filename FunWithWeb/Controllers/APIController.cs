fac6df0c35fa783ec39f5d2ebb300180144f83b8
Conner Martin <connersmartin@gmail.com>
2017-09-14 22:36:20 -0700

    Thinking about basic desgin for next steps

--- FunWithWeb/Controllers/APIController.cs
+++ FunWithWeb/Controllers/APIController.cs
@@ -11,14 +11,26 @@ namespace FunWithWeb.Controllers
 {
     public class APIController : Controller
     {
+        /* Overall design goal would be:
+         * Auth/Search page
+         * brings results in
+         * 
+         * stretch goal play files
+         * search from DataTest info
+         */
+
+
         // GET: API
+        // just figuring out how the API works 
         public async Task<ActionResult> Detail()
         {
+
+            //need to figure out how to call method from index view
             Spotify s = new Spotify();
            
             s.SpotAuth();
 
-
+            //figure out how to implement this better
             s.Artist = s.Performer();
             s.TrackName = s.Track();
 
@@ -27,6 +39,7 @@ namespace FunWithWeb.Controllers
 
         public ActionResult Index()
         {
+
             return View();
         }
 
